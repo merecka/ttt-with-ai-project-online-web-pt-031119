c6f8b55984d7be47a3b319db79b9c3d78264fc8a
Alex Merecka <merecka@gmail.com>
2019-05-13 03:32:27 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -11,4 +11,24 @@ class Board
     self.cells = Array.new(9, " ")
   end
 
+  def display
+    puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
+    puts "-----------"
+    puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
+    puts "-----------"
+    puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
+  end
+
+  def position(value)
+    self.cells[value.to_i - 1]
+  end
+
+  def full?
+    if self.cells.include?(" ")
+      false
+    else
+      true
+    end
+  end
+
 end
